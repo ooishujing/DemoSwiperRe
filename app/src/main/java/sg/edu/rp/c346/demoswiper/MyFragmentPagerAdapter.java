c2d6abd9bb05b2e0ca770110c2e2ac6aa919a987
ooishujing <18016012@myrp.edu.sg>
2021-05-27 11:21:03 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/demoswiper/MyFragmentPagerAdapter.java
@@ -0,0 +1,28 @@
+package sg.edu.rp.c346.demoswiper;
+
+import java.util.ArrayList;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class MyFragmentPagerAdapter extends FragmentPagerAdapter {
+
+    ArrayList<Fragment> fragments;
+
+    public MyFragmentPagerAdapter(FragmentManager fm, ArrayList<Fragment> al) {
+        super(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        fragments = al;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragments.size();
+    }
+
+}
